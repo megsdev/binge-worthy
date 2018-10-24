e30d541c7ffe09b597a2ebca2f40ea57d88cb9cc
Megan Duclos <37349288+megsdev@users.noreply.github.com>
2018-10-23 20:57:50 -0600

    change localhost to DO Ip and run build

--- server/index.js
+++ server/index.js
@@ -1,57 +1,54 @@
-require('dotenv').config({ path: `${__dirname}/.env` })
-const bodyParser = require('body-parser')
-const massive = require('massive')
-const controller = require('./controller')
-const cors = require('cors')
-const passport = require('passport')
-const strategy = require(`${__dirname}/strategy.js`)
-const session = require('express-session')
-const app = require('./app');
+require("dotenv").config({ path: `${__dirname}/.env` });
+const bodyParser = require("body-parser");
+const massive = require("massive");
+const controller = require("./controller");
+const cors = require("cors");
+const passport = require("passport");
+const strategy = require(`${__dirname}/strategy.js`);
+const session = require("express-session");
+const app = require("./app");
 
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cors());
 
-app.use(session({
-  secret: 'shitballs',
-  resave: false,
-  saveUninitialized: false
-}))
+app.use(
+  session({
+    secret: "shitballs",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(strategy)
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
 
-passport.serializeUser(function (user, done) {
-
+passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
-  let db = app.get('db')
-  db.getUser([user.user_id])
-    .then(user => {
-      done(null, user[0])
-    })
+passport.deserializeUser(function(user, done) {
+  let db = app.get("db");
+  db.getUser([user.user_id]).then(user => {
+    done(null, user[0]);
+  });
 });
 
 //AUTH0
-app.get('/login',
-  passport.authenticate('auth0',
-  ),
-  (req, res) => {
-    // console.log('req.user', req.user)
-  }
+app.get("/login", passport.authenticate("auth0"), (req, res) => {
+  // console.log('req.user', req.user)
+});
+
+app.get(
+  "/login/callback",
+  passport.authenticate("auth0", {
+    successRedirect: "165.227.10.75:3000/",
+    failureRedirect: "165.227.10.75:3000/login",
+    failureFlash: false
+  })
 );
 
-app.get('/login/callback', passport.authenticate('auth0', {
-  successRedirect: 'http://localhost:3000/',
-  failureRedirect: 'http://localhost:3000/login',
-  failureFlash: false
-
-}))
-
-app.get('/me', (req, res, next) => {
+app.get("/me", (req, res, next) => {
   if (!req.user) {
     res.sendStatus(401);
   } else {
@@ -60,10 +57,12 @@ app.get('/me', (req, res, next) => {
 });
 
 //ENDPOINTS
-app.post('/api/list', controller.addShow)
-app.get('/api/list', controller.getShows)
-app.put('/api/list', controller.moveShow)
-app.delete('/api/list', controller.deleteShow)
+app.post("/api/list", controller.addShow);
+app.get("/api/list", controller.getShows);
+app.put("/api/list", controller.moveShow);
+app.delete("/api/list", controller.deleteShow);
 
-const port = 4000
-app.listen(port, () => { console.log(`server listening on port ${port}`) })
+const port = 4000;
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
