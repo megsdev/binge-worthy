1c420fa503aacf1b4945453096bb70a9fd7fc701
Megan Duclos <37349288+megsdev@users.noreply.github.com>
2018-10-24 18:38:03 -0600

    edit button

--- client/src/components/Button/EditButton.js
+++ client/src/components/Button/EditButton.js
@@ -1,38 +1,66 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
 class EditButton extends Component {
+  moveShow = (showId, listType) => {
+    axios({
+      method: "PUT",
+      url: "/api/list",
+      data: {
+        tmdb_id: showId,
+        list_type: listType
+      }
+    });
 
-    moveShow = (showId, listType) => {
-        axios({
-            method: 'PUT',
-            url: '/api/list',
-            data: {
-                tmdb_id: showId,
-                list_type: listType
-            }
-        })
-    }
+    axios({
+      method: "GET",
+      url: "/api/list"
+    }).then(response => {
+      if (this.props.userShows) {
+        this.props.resetUserShows();
+      }
+      response.data.filter(show => {
+        if (show.list_type === "Binged") {
+          fetchShow(show.tmdb_id).then(response => {
+            this.props.updateBinged(response.data);
+          });
+        } else if (show.list_type === "Currently Bingeing") {
+          fetchShow(show.tmdb_id).then(response => {
+            this.props.updateCurrentlyBingeing(response.data);
+          });
+        } else {
+          fetchShow(show.tmdb_id).then(response => {
+            this.props.updateWannaBinge(response.data);
+          });
+        }
+      });
+    });
+  };
 
-    handleMoveShow = (type) => (event) => {
-        event.preventDefault();
-        this.moveShow(this.props.id, type);
-        window.location.reload();
-    }
+  handleMoveShow = type => event => {
+    event.preventDefault();
+    this.moveShow(this.props.id, type);
+    // window.location.reload();
+  };
 
-    render() {
-        return (
-            <div className='dropdown' >
-                <button className='dropdown-button' >Move Show</button>
-                <div className='dropdown-content'>
-                    <button onClick={this.handleMoveShow('Wanna Binge')} href="#">Wanna Binge</button>
-                    <button onClick={this.handleMoveShow('Currently Bingeing')} href="#">Currently Bingeing</button>
-                    <button onClick={this.handleMoveShow('Binged')} href="#">Binged</button>
-                </div>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className="dropdown">
+        <button className="dropdown-button">Move Show</button>
+        <div className="dropdown-content">
+          <button onClick={this.handleMoveShow("Wanna Binge")} href="#">
+            Wanna Binge
+          </button>
+          <button onClick={this.handleMoveShow("Currently Bingeing")} href="#">
+            Currently Bingeing
+          </button>
+          <button onClick={this.handleMoveShow("Binged")} href="#">
+            Binged
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default EditButton
+export default EditButton;
